2394fca9fdbcd1c247c43563376e043f38c87cc0
eddy0 <awindy369@gmail.com>
2018-07-29 21:15:22 -0700

    webpack config

---
+++ webpack/babel/webpackforcss.js
@@ -0,0 +1,23 @@
+const path = require('path')
+const {getIfUtils} = require('webpack-config-utils')
+
+module.exports = (env) => {
+    // 为了把参数信息显示出来, 否则 webpack 只会显示 id
+    const {ifProd, ifNotProd} = getIfUtils
+    return {
+        entry: 'index.js',
+        output: {
+            path: path.resolve('dist'),
+            filename: 'bundle.js',
+            // for webpack-dev-server
+            publicPath: '/dist/',
+        },
+        devtool: ifProd('source-map', 'eval'),
+        module: {
+            use: [
+                {test: /\.js$/, loader: ['babel-loader'], exclude:/node_modules/, },
+                    {test: /\.css$/, loader: ['css-loader', 'style-loader'] },
+            ],
+        }
+    }
+}
